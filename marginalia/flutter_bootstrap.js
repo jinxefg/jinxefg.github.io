8c89b873d83c077db7c3e17db5911d77b5ac4d23
Astraea <astraea@users.noreply.github.com>
2026-07-10 11:36:39 -0500

    Deploy Marginalia build 10: periwinkle, Sky tab, month/year badges

--- marginalia/flutter_bootstrap.js
+++ marginalia/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1609195859" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3875452133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
