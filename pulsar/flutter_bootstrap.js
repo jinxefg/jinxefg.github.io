b75c0b4e6563ec9d19332514276ab53a7ca448f7
Astraea <astraea@users.noreply.github.com>
2026-07-11 09:25:47 -0500

    Deploy Pulsar build 4 — the journey dot becomes a tiny star

--- pulsar/flutter_bootstrap.js
+++ pulsar/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1509626728" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "411488197" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
